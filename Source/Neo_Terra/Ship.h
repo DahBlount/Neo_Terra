56a83b259d19668385aca115e7dd4277b4aada72
DahBlount <tylerrblount@gmail.com>
2015-06-08 09:29:25 -0700

    Start work on Ship class

---
+++ Source/Neo_Terra/Ship.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "Ship.generated.h"
+
+UCLASS()
+class NEO_TERRA_API AShip : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	struct SHIP
+	{
+		FVector Pos;
+
+	};
+
+public:
+	// Sets default values for this pawn's properties
+	AShip();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	
+	
+};
